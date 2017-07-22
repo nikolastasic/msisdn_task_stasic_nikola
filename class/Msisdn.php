086feb6f49c574441e1ebabf2f36da9b41c7f11a
nikolastasic <nklstasic93@gmail.com>
2017-07-22 15:01:02 +0200

    Adding starting folders and files

---
+++ class/Msisdn.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vladimir Stašić
+ * Date: 7/22/2017
+ * Time: 2:59 PM
+ */
+
+class Msisdn
+{
+
+}
---
+++ db_connect.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikola Stašić
+ * Date: 7/22/2017
+ * Time: 2:58 PM
+ */
