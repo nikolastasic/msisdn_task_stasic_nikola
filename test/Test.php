e022e76e1dce74df7103442bf5ad8970c31f4086
nikolastasic <nklstasic93@gmail.com>
2017-07-23 15:02:09 +0200

    Installing PHPUnit

---
+++ test/Test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikola Stašić
+ * Date: 7/23/2017
+ * Time: 2:45 PM
+ */
+
+require_once  ('../vendor/autoload.php');
+require_once ('../class/Msisdn.php');
+
+class Test extends PHPUnit_Framework_TestCase
+{
+
+}
